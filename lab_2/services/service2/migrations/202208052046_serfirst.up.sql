e8648e1cc0a6399beae5b05d1843f6a95c7cb052
nastiabondarenko17@gmail.com <nastiabondarenko17@gmail.com>
2022-05-08 21:43:39 +0300

    "add crud and migrations"

---
+++ lab_2/services/service2/migrations/202208052046_serfirst.up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE serfirst(
+  id int ,
+  name varchar(50) 
+);
+
+INSERT INTO serfirst VALUES (1, 'one');
+INSERT INTO serfirst VALUES (2, 'two');
+INSERT INTO serfirst VALUES (3, 'three');
+INSERT INTO serfirst VALUES (4, 'four');
+INSERT INTO serfirst VALUES (5, 'five');
---
+++ lab_2/services/service2/src/config.js
@@ -0,0 +1,10 @@
+const config = {
+    user: 'nastiabondarenko',
+    host: 'localhost',
+    database: 'postgres2',
+    password: '1',
+    port: 5432,
+  };
+
+
+export {config};  
