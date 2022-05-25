830086dd2a2f6d9c6ba9d044c67c0ded289aed68
Vladyslav Rybak <vladyslavry141@gmail.com>
2022-05-25 21:18:57 +0300

    quick-fix(lab_2.services): fix invalid port and host in service2

--- lab_2/services/service2/src/server.js
+++ lab_2/services/service2/src/server.js
@@ -6,8 +6,8 @@ import express from "express";
 let conect = new Connection();
 let crud = new CRUD(conect, config);
 
-const port = process.env["SERVICE2_SERVICE_SERVICE_PORT"];
-const host = process.env["SERVICE2_SERVICE_SERVICE_PORT"];
+const port = 8080;
+const host = "0.0.0.0";
 
 const app = express();
 
@@ -20,40 +20,44 @@ app.get("/", (req, res) => {
 
 app.post("/serfirst/get", urlencodedParser, async (request, response) => {
   try {
+    console.log("/serfirst/get", JSON.stringify(request.body));
     let result = await crud.select(request.body.id);
     response.send({ status: "OK", data: result });
   } catch (error) {
-    console.error(error);
+    console.log(error);
     response.send({ status: "error" });
   }
 });
 
 app.post("/serfirst/insert", urlencodedParser, async (request, response) => {
   try {
+    console.log("/serfirst/insert", JSON.stringify(request.body));
     await crud.insert(request.body.id, request.body.name);
     response.send({ status: "OK" });
   } catch (error) {
-    console.error(error);
+    console.log(error);
     response.send({ status: "error" });
   }
 });
 
 app.post("/serfirst/delete", urlencodedParser, async (request, response) => {
   try {
+    console.log("/serfirst/delete", JSON.stringify(request.body));
     await crud.delete(request.body.id);
     response.send({ status: "OK" });
   } catch (error) {
-    console.error(error);
+    console.log(error);
     response.send({ status: "error" });
   }
 });
 
 app.post("/serfirst/update", urlencodedParser, async (request, response) => {
   try {
+    console.log("/serfirst/update", JSON.stringify(request.body));
     await crud.update(request.body.id, request.body.name);
     response.send({ status: "OK" });
   } catch (error) {
-    console.error(error);
+    console.log(error);
     response.send({ status: "error" });
   }
 });
