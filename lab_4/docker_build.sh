fb188b048362ff6c1199649f5cf9858eee0b9206
Vladyslav Rybak <vladyslavry141@gmail.com>
2022-06-26 22:54:10 +0300

    fix: fix versions in docker_build.sh

--- lab_4/docker_build.sh
+++ lab_4/docker_build.sh
@@ -1,5 +1,5 @@
 
 eval $(minikube docker-env)
-docker build -t service1:0.2 -f services/service1/Dockerfile services/service1
-docker build -t service2:0.1 -f services/service2/Dockerfile services/service2
-docker build -t service2-migrations:0.1 -f services/service2/migrations/Dockerfile services/service2/migrations/
+docker build -t service1:0.1.0 -f services/service1/Dockerfile services/service1
+docker build -t service2:0.2.0 -f services/service2/Dockerfile services/service2
+docker build -t service2-migrations:0.1.0 -f services/service2/migrations/Dockerfile services/service2/migrations/
