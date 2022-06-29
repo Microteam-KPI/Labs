b9d87651b00b02dd99f78e3a506270ab08ab1feb
Vladyslav Rybak <vladyslavry141@gmail.com>
2022-06-29 21:22:28 +0300

    fix: fix docker_build.sh

--- lab_4/docker_build.sh
+++ lab_4/docker_build.sh
@@ -1,5 +1,6 @@
 
-eval $(minikube docker-env)
+#!/bin/bash
+minikube -p minikube docker-env | source
 docker build -t service1:0.1.0 -f services/service1/Dockerfile services/service1
 docker build -t service2:0.2.0 -f services/service2/Dockerfile services/service2
 docker build -t service2-migrations:0.1.0 -f services/service2/migrations/Dockerfile services/service2/migrations/
